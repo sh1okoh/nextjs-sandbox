d45811558f3800d0356aa952e348da8dcd5481eb
sh1okoh <kooooo.0719task@gmail.com>
2022-04-06 00:01:22 +0900

    add

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'JIT',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+  ]
+}
