e2e979102378293070573da00a4307f0e9f6cf75
sh1okoh <kooooo.0719task@gmail.com>
2022-04-07 00:27:42 +0900

    add

--- front/pages/counter.tsx
+++ front/pages/counter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import counterSlice from '../ducks/counter/slice';
 import { useCounterState } from '../ducks/counter/selectors';
 
