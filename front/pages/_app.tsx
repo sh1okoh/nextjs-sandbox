cec02734f36cacc69ecb718e4de00cab9b1b4556
sh1okoh <kooooo.0719task@gmail.com>
2022-04-06 21:12:25 +0900

    add

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import createStore from '../ducks/createStore';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Provider store={createStore()}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
---
+++ front/pages/counter.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import counterSlice from '../ducks/counter/slice';
+import { useCounterState } from '../ducks/counter/selectors';
+
