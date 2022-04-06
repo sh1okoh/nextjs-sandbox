7dfbd91f756593747a341bd6aaf3082633a68a6b
sh1okoh <kooooo.0719task@gmail.com>
2022-04-06 21:47:26 +0900

    add

--- front/pages/counter.tsx
+++ front/pages/counter.tsx
@@ -3,3 +3,30 @@ import { useDispatch } from 'react-redux';
 import counterSlice from '../ducks/counter/slice';
 import { useCounterState } from '../ducks/counter/selectors';
 
+const CounterPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const state = useCounterState().counter;
+
+  const onClickIncrement = () => {
+    dispatch(counterSlice.actions.incrementCounter(1));
+  }
+
+  const onClickDecrement = () => {
+    dispatch(counterSlice.actions.decrementCounter(1));
+  }
+
+  return (
+    <>
+      <button type='button' onClick={onClickIncrement}>
+        増やす
+      </button>
+      <button type='button' onClick={onClickDecrement}>
+        減らす
+      </button>
+      <p>ねこが{state.count}匹いる</p>
+    </>
+  )
+
+}
+
+export default CounterPage;
