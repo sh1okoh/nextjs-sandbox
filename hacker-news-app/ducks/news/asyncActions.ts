355b30d48c82ef5143e868a0a9c38d17111f145d
sh1okoh <kooooo.0719task@gmail.com>
2022-04-09 00:11:23 +0900

    add

--- hacker-news-app/ducks/news/asyncActions.ts
+++ hacker-news-app/ducks/news/asyncActions.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
 export const asyncFetchNews = createAsyncThunk<string, string>(
@@ -11,3 +12,13 @@ export const asyncFetchNews = createAsyncThunk<string, string>(
     return res;
   }
 )
+
+export const asyncFetchNewById = createAsyncThunk<string, string>(
+  'news/fetchNewsById',
+  async (arg: string): Promise<string> => {
+    const endpoint = 'https://hacker-news.firebaseio.com/v0/item/'
+    const res = axios.post(endpoint, )
+    return arg
+  }
+)
+
